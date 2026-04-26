b888e3034d9334a53f3da7066ee0d54459c03aae
neelavathy g <neelavathysangeetha@gmail.com>
2026-04-26 21:48:09 +0530

    Day 3 - state mgmt in parent component

--- Tic-Tac-Toe/src/App.tsx
+++ Tic-Tac-Toe/src/App.tsx
@@ -1,37 +1,39 @@
 import { useState } from "react";
+import './App.css';
 
-function Square(){
-  const [value, setValue] = useState(null);
-
-  function handleClick(){
-    setValue("X");
-  }
-  
+function Square({value, OnSquareClick}){
   return (
     <>
-      <button className="square" onClick={handleClick}>{value}</button>
+      <button className="square" onClick={OnSquareClick}>{value}</button>
     </>
   );
   
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
   return(
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} OnSquareClick={() =>handleClick(0)}/>
+        <Square value={squares[1]} OnSquareClick={() =>handleClick(1)}/>
+        <Square value={squares[2]} OnSquareClick={() =>handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />  
+        <Square value={squares[3]} OnSquareClick={() =>handleClick(3)}/>
+        <Square value={squares[4]} OnSquareClick={() =>handleClick(4)}/>
+        <Square value={squares[5]} OnSquareClick={() =>handleClick(5)}/>  
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />  
+        <Square value={squares[6]} OnSquareClick={() =>handleClick(6)}/>
+        <Square value={squares[7]} OnSquareClick={() =>handleClick(7)}/>
+        <Square value={squares[8]} OnSquareClick={() =>handleClick(8)}/>  
       </div>
     </>
   );
